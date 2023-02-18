4350c922f5c971518d2d5baca30904f9cc0b3bd5
Nikolay Savchenko <7548568@gmail.com>
2023-02-18 16:34:31 +0300

    Add data-migration with tariffs

--- tgbot/admin.py
+++ tgbot/admin.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 
 from .models import *
 
+
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = (
         'telegram',
         'name',
     )
+
 
 admin.site.register(Developer)
 admin.site.register(Manager)
